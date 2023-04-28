d32435a2b562aa6c6cea4af9eadac3665d1ef25c
Yago <yago.pereira@estudante.ufjf.br>
2023-04-28 06:30:51 -0300

    Javascript: Arrays

---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,7 @@
+const alunos = ['João', 'Juliana', 'Ana', 'Caio'];
+const media = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, media];
+
+console.log(`A aluna da posição 1 da lista de alunos é ${listaDeAlunosEMedias[0][1]}.
+A média dessa aluna é: ${listaDeAlunosEMedias[1][1]}.`);
