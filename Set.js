34b58381b8469517c0eab270393d5a6549d00259
Yago <yago.pereira@estudante.ufjf.br>
2023-05-05 20:47:49 -0300

    Concluindo o curso de Arrays

---
+++ Set.js
@@ -0,0 +1,9 @@
+const nomes = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"]
+
+const meuSet = new Set(nomes)
+
+console.log(meuSet)
+
+const novaLista = new Array(...meuSet)
+
+console.log(novaLista)
